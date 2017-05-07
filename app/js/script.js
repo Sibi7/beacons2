a993088683ec6fd3b98bd5da7fb67278d049c9b4
Vladimir Lipodat <sibi_7@mail.ru>
2017-05-07 14:03:05 +0300

    add counter

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,54 @@
+$(document).on('click', '.plus', function () {
+    event.preventDefault();
+    var count = $('.counter, .counter').find('.number'),
+        val = parseInt($('.counter').find('.number').val());
+    if (val == 999) {
+        return false;
+    } else {
+        count.val(val + 1);
+        $('.js-single-addtocart').attr('data-quantity', count.val());
+        $('.js-single-favorites').attr('data-quantity', count.val());
+    }
+    return false;
+});
+
+$(document).on('click', '.minus', function () {
+    event.preventDefault();
+    var count = $('.counter').find('.number');
+    var counter = parseInt(count.val()) - 1;
+    counter = counter < 1 ? 1 : counter;
+    count.val(counter);
+    count.change();
+    $('.js-single-addtocart').attr('data-quantity', counter);
+    $('.js-single-favorites').attr('data-quantity', counter);
+    return false;
+});
+$(document).on('click', '.plus', function () {
+    event.preventDefault();
+    var count = $('.counter1, .counter1').find('.number'),
+        val = parseInt($('.counter1').find('.number').val());
+    if (val == 999) {
+        return false;
+    } else {
+        count.val(val + 1);
+        $('.js-single-addtocart').attr('data-quantity', count.val());
+        $('.js-single-favorites').attr('data-quantity', count.val());
+    }
+    return false;
+});
+
+$(document).on('click', '.minus', function () {
+    event.preventDefault();
+    var count = $('.counter1').find('.number');
+    var counter = parseInt(count.val()) - 1;
+    counter = counter < 1 ? 1 : counter;
+    count.val(counter);
+    count.change();
+    $('.js-single-addtocart').attr('data-quantity', counter);
+    $('.js-single-favorites').attr('data-quantity', counter);
+    return false;
+});
+
 // $(document).ready(function () {
 
 //     /*header mobile menu*/
